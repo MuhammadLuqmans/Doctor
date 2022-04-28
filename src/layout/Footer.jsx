8f8a6aaf922e51258a1eae0a5e96aef7e40729d2
MuhammadLuqmans <musa60209@gmail.com>
2022-04-29 04:57:02 +0500

    complete navbar

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div><footer class="footer bg-dark text-white mt-20">
-    <p>Copyright &copy;2021 - Powered by <a href="https://alfiofedericodp.netlify.app/" target="_blank" class="mypage" rel="noreferrer">Federico</a></p>
+    <div><footer className="footer bg-dark text-white mt-20">
+    <p>Copyright &copy;2021 - Powered by <a href="https://alfiofedericodp.netlify.app/" target="_blank" className="mypage" rel="noreferrer">Federico</a></p>
   </footer></div>
   )
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,45 +1,104 @@
-import React from 'react';
-import './Navbar.css'
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
 
 export default function Navbar() {
 
-    const navbar = document.getElementById('navbar')
-let scrolled = false;
+  const [navSize, setnavSize] = useState("translateY(0px)");
+  const [navColor, setnavColor] = useState("transparent");
 
-window.onscroll = function(){
-  if(window.pageYOffset > 100){
-    navbar.classList.remove('top');
-    if(!scrolled){
-      navbar.style.transform = 'translateY(-70px)'
+  const listenScrollEvent = () => {
+
+
+    if (window.pageYOffset < 100) {
+    
+      setnavSize("translateY(-70px)");
+      
+      setTimeout(function () {
+        setnavSize("translateY(0)");
+     
+      }, 200);
+    } else {
+      setnavSize("translateY(0px)");
+      setnavColor('#456edf')
     }
-    setTimeout(function(){
-      navbar.style.transform = "translateY(0)";
-      scrolled = true
-    },200)
-  }else {
-    navbar.classList.add('top')
-    scrolled = false
-  }
-}
-
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
+    };
+  }, []);
 
   return (
     <div>
-        <header className="hero" id='home'>
-    <div className="navbar top border-3" id="navbar">
-      <h1 className="logo text-[35px] text-left">
-        <span className="text text-[35px] text-[#28a745]"><i className="fas fa-user-md"></i></span> Dr. Pietro Lerro
-        <hr /> <small className="specialized">Specialista in Pediatria, Pediatra Gastroenterologo </small>
-      </h1>
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">Info</a></li>
-          <li><a href="#contact">Contatti</a></li>
-        </ul>
-      </nav>
+      <header className="hero" id="home">
+        <div
+          className="navbar top border-3"
+          id="navbar"
+          style={{ transform: navSize, background:navColor }}
+        >
+          <h1 className="logo text-[35px] text-left">
+            <span className="text text-[35px] text-[#28a745]">
+              <i className="fas fa-user-md"></i>
+            </span>{" "}
+            Dr. Pietro Lerro
+            <hr />{" "}
+            <small className="specialized">
+              Specialista in Pediatria, Pediatra Gastroenterologo{" "}
+            </small>
+          </h1>
+          <nav>
+            <ul>
+              <li>
+                <a href="#home" >Home</a>
+              </li>
+              <li>
+                <a href="#about" >Info</a>
+              </li>
+              <li>
+                <a href="#contact">Contatti</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </div>
-  </header>
-  </div>
-  )
+  );
 }
+
+// import React, { useState, useEffect } from "react";
+// import "./Navbar.css";
+// export default function App() {
+//   const [navSize, setnavSize] = useState("translateY(0px)");
+//   const [navColor, setnavColor] = useState("transparent");
+//   const listenScrollEvent = () => {
+//     // window.scrollY > 10 ? setnavColor("#252734") : setnavColor("transparent");
+//     window.scrollY > 10 ? setnavSize("translateY(-70px)") : setnavSize("translateY(0px)");
+//   };
+//   useEffect(() => {
+//     window.addEventListener("scroll", listenScrollEvent);
+//     return () => {
+//       window.removeEventListener("scroll", listenScrollEvent);
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <nav
+//         style={{
+//           backgroundColor: navColor,
+//           transform: navSize,
+//           transition: "all 1s",
+//         }}
+//       >
+//         <ul>
+//           <li>Home</li>
+//           <li>About</li>
+//           <li>Project</li>
+//           <li>Skills</li>
+//           <li>Contact </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// }
